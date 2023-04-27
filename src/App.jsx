50a097faf6bc59853f64f6631e696c893c97d358
Barun Debnath <barundebnath91@gmail.com>
2023-04-27 23:20:21 +0530

    tailwind variables added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
 export default function App() {
-  return <h1 className="text-5xl font-bold ">Hello world!</h1>;
+  return <h1 className="text-5xl font-bold font-display ">Hello world!</h1>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ["manrope", "sans"],
+    },
+    colors: {
+      bgPrimary: "#B4EE91",
+      buttonBg: "#013106",
+      buttonSecondaryBg: "#D9D9D9",
+    },
   },
   plugins: [],
 };
